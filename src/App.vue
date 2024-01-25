f3697859bf57a940b16cbfd4e9e4713712fbc60c
linkon63 <m.alinkon10@gmail.com>
2024-01-25 16:47:51 +0600

    Changes the filename

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   <!-- <InputBoxTest /> -->
 </template>
 <script setup lang="ts">
-import SelectInputTest from "./components/form-test/SelectInputTest.vue";
-import TextAreaTest from "./components/form-test/TextAreaTest.vue";
+import SelectInputTest from "./components/reusable-component-playground/SelectInputTest.vue";
+import TextAreaTest from "./components/reusable-component-playground/TextAreaTest.vue";
 import BaseTextArea from "./components/reusable-components/BaseTextArea.vue";
 </script>
