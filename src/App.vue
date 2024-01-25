91e0a2af4d2a00fe565f9c54324c768db649ba8e
linkon63 <m.alinkon10@gmail.com>
2024-01-25 13:03:08 +0600

    dropdown select

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template class="w-full">
-  <TextAreaTest />
+  <DropDownTest />
+  <!-- <TextAreaTest /> -->
   <!-- <CheckboxTest /> -->
   <!-- <InputBoxTest /> -->
 </template>
 <script setup lang="ts">
+import DropDownTest from "./components/form-test/DropDownTest.vue";
 import TextAreaTest from "./components/form-test/TextAreaTest.vue";
 import BaseTextArea from "./components/reusable-components/BaseTextArea.vue";
 </script>
---
+++ src/components/form-test/DropDownTest.vue
@@ -0,0 +1,9 @@
+<template>
+  <BaseDropDown />
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import BaseDropDown from "../reusable-components/BaseDropDown.vue";
+
+const message = ref("Write your message");
+</script>
