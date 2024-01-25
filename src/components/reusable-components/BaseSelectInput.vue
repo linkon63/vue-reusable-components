c7b10e36e241bdf5fb3b9f5b6716c8af9f7842ad
linkon63 <m.alinkon10@gmail.com>
2024-01-25 14:49:51 +0600

    changes the file

--- src/components/reusable-components/BaseSelectInput.vue
+++ src/components/reusable-components/BaseSelectInput.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-6/12 p-5">
     <label
-      for="default"
+      for="default-select-input"
       class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
       :class="labelClass"
       >{{ label }}</label
     >
     <select
-      id="default"
+      id="default-select-input"
       class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       :class="class"
       :name="name"
