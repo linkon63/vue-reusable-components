792a162dc006eff454ef4d96c39a8a85fa18cda2
linkon63 <m.alinkon10@gmail.com>
2024-01-25 19:01:58 +0600

    package file created

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import BaseCheckboxVue from "./components/reusable-components/BaseCheckbox.vue";
+import BaseDropDownVue from "./components/reusable-components/BaseDropDown.vue";
+import BaseInput from "./components/reusable-components/BaseInput.vue";
+import BaseRadioVue from "./components/reusable-components/BaseRadio.vue";
+import BaseSelectInputVue from "./components/reusable-components/BaseSelectInput.vue";
+import BaseTextAreaVue from "./components/reusable-components/BaseTextArea.vue";
+
+export {
+  BaseCheckboxVue,
+  BaseDropDownVue,
+  BaseInput,
+  BaseRadioVue,
+  BaseSelectInputVue,
+  BaseTextAreaVue,
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      // src/index.ts is where we have exported the component(s)
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "VueReusableComponents",
+      // the name of the output files when the build is run
+      fileName: "vue-reusable-components",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 })
