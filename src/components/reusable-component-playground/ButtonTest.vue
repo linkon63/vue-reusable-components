92458962b7ab55b284e4efd769ece4b84c5f7cda
linkon63 <m.alinkon10@gmail.com>
2024-01-26 11:20:08 +0600

    button readme update

--- src/components/reusable-component-playground/ButtonTest.vue
+++ src/components/reusable-component-playground/ButtonTest.vue
@@ -10,8 +10,8 @@
       name="Paypal Payment"
       class="text-white bg-[#050708] hover:bg-[#050708]/80 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#050708]/40 dark:focus:ring-gray-600 me-2 mb-2"
       @on-click="console.log('log')"
-      >
-      <!-- :disabled="true" -->
+      :disabled="false"
+    >
       <template #prefix>
         <svg
           class="w-5 h-5 me-2 -ms-1"
