c339e09ad7fe39ed6c5c3b4f9d4d929820232c07
linkon63 <m.alinkon10@gmail.com>
2024-01-30 14:15:30 +0600

    ----------

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,6 @@
 <template class="w-full">
   <BaseModal />
-  <!-- <BaseModalTest /> -->
-  <!-- <ButtonTest /> -->
-  <!-- <RadioboxTest /> -->
-  <!-- <SelectInputTest /> -->
-  <!-- <TextAreaTest /> -->
-  <!-- <CheckboxTest /> -->
-  <!-- <InputBoxTest /> -->
 </template>
 <script setup lang="ts">
-import { BaseModal } from "vue-reusable-components";
-import BaseModalTest from "./components/reusable-component-playground/BaseModalTest.vue";
-import ButtonTest from "./components/reusable-component-playground/ButtonTest.vue";
-import RadioboxTest from "./components/reusable-component-playground/RadioboxTest.vue";
-import SelectInputTest from "./components/reusable-component-playground/SelectInputTest.vue";
-import TextAreaTest from "./components/reusable-component-playground/TextAreaTest.vue";
-import BaseTextArea from "./components/reusable-components/BaseTextArea.vue";
+import BaseModal from "vue-reusable-components";
 </script>
