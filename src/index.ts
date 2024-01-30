3e18b8d422986a314caf216a91992145b033ab3e
linkon63 <m.alinkon10@gmail.com>
2024-01-30 14:11:34 +0600

    ------

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@ import BaseInput from "./components/reusable-components/BaseInput.vue";
 import BaseRadioVue from "./components/reusable-components/BaseRadio.vue";
 import BaseSelectInputVue from "./components/reusable-components/BaseSelectInput.vue";
 import BaseTextAreaVue from "./components/reusable-components/BaseTextArea.vue";
+import BaseButtonVue from "./components/reusable-components/BaseButton/BaseButton.vue";
+import BaseModalVue from "./components/reusable-components/BaseModal/BaseModal.vue";
 
 export default {
-  BaseCheckboxVue,
-  BaseDropDownVue,
   BaseInput,
-  BaseRadioVue,
-  BaseSelectInputVue,
+  BaseCheckboxVue,
   BaseTextAreaVue,
+  BaseSelectInputVue,
+  BaseDropDownVue,
+  BaseRadioVue,
+  BaseButtonVue,
+  BaseModalVue,
 };
