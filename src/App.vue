2d06064fcab7b581c02dbffacfd9c0a5da2ec619
linkon63 <m.alinkon10@gmail.com>
2024-01-30 14:34:35 +0600

    ---------

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template class="w-full">
   <h1>Library Testing v-1</h1>
   <BaseInput />
+  <BaseCheckbox />
+  <h1>Library Testing v-2</h1>
 </template>
 <script setup lang="ts">
-import { BaseModal, BaseInput } from "vue-reusable-components";
+import { BaseInput, BaseCheckbox } from "vue-reusable-components";
 </script>
