83bac839bb7c5eda74c417fb6ef5481e45e2d3ed
linkon63 <m.alinkon10@gmail.com>
2024-02-01 11:17:20 +0600

    Base modal expale added

--- src/components/reusable-components/BaseToggle/BaseToggle.vue
+++ src/components/reusable-components/BaseToggle/BaseToggle.vue
@@ -10,36 +10,23 @@
       class="sr-only peer"
       :disabled="disabled"
       :required="required"
+      :checked="checked"
       @click="click"
       @change="changes"
     />
     <div
       class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
+      :class="toggleClasses"
     ></div>
     <span
       :class="toggleBallClasses"
       class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-      >{{ label }}</span
     >
+      <slot name="labelPrefix" />
+      {{ label }}
+      <slot name="labelSuffix" />
+    </span>
   </label>
-
-  <!-- <label
-    class="w-fit relative inline-flex items-center cursor-pointer"
-    :class="labelClasses"
-  >
-    <input
-      v-model="model"
-      :disabled="disabled"
-      class="sr-only peer"
-      type="checkbox"
-    />
-    <span :class="toggleClasses" />
-    <span
-      class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-      :class="toggleBallClasses"
-      >{{ label }}</span
-    >
-  </label> -->
 </template>
 
 <script lang="ts" setup>
