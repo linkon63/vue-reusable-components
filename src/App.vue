65f465004143255d36478ca7d251829d3c8c6187
linkon63 <m.alinkon10@gmail.com>
2024-02-01 13:39:56 +0600

    Global testing working

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template class="w-full">
   <h1>Testing Components from here Start</h1>
   <!-- <KCIcon /> -->
-  <FccButton text="Run the Tests" />
-  <FccImage text="Run the 222"></FccImage>
   <BaseButton name="hello linkon" />
+  <BaseCheckbox />
   <h1>Testing Components from here End</h1>
 </template>
 <script setup lang="ts">
-import { FccButton, FccImage, BaseButton } from "kon-component-lib";
-import "kon-component-lib/style.css";
+import { BaseButton, BaseCheckbox } from "vue-reusable-components";
 // import KCIcon from "kon-components";
 </script>
