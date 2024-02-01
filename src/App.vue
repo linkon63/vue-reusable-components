15a60b37c5f7a9d56a76df1d0d0b23a39a250499
linkon63 <m.alinkon10@gmail.com>
2024-02-01 15:10:22 +0600

    chips added for the components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
 <template class="w-full">
   <h1>Testing Components from here</h1>
   <div class="p-8">
-    <BaseToggle
-      label="Hello Toggle"
-      toggle-classes="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-red-300 dark:peer-focus:ring-red-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"
-      toggle-ball-classes="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-    />
-    <BaseToggle label="Sound" color="green">
-      <template #labelPrefix>📢</template>
-    </BaseToggle>
+    <BaseChips />
   </div>
-
-  <!-- <BaseModalTest /> -->
-  <!-- <ButtonTest /> -->
-  <!-- <RadioboxTest /> -->
-  <!-- <SelectInputTest /> -->
-  <!-- <TextAreaTest /> -->
-  <!-- <CheckboxTest /> -->
-  <!-- <InputBoxTest /> -->
 </template>
 <script setup lang="ts">
-import BaseModalTest from "./components/reusable-component-playground/BaseModalTest.vue";
-import ButtonTest from "./components/reusable-component-playground/ButtonTest.vue";
-import RadioboxTest from "./components/reusable-component-playground/RadioboxTest.vue";
-import SelectInputTest from "./components/reusable-component-playground/SelectInputTest.vue";
-import TextAreaTest from "./components/reusable-component-playground/TextAreaTest.vue";
-import BaseTextArea from "./components/reusable-components/BaseTextArea.vue";
-import BaseToggle from "./components/reusable-components/BaseToggle/BaseToggle.vue";
+import BaseChips from "./components/reusable-components/BaseChips/BaseChips.vue";
 </script>
---
+++ src/components/reusable-components/BaseChips/BaseChips.vue
@@ -0,0 +1,63 @@
+<template>
+  <span
+    id="badge-dismiss-default"
+    class="inline-flex items-center px-2 py-1 me-2 text-sm font-medium text-blue-800 bg-blue-100 rounded dark:bg-blue-900 dark:text-blue-300"
+    :class="labelClass"
+    v-if="visibility"
+  >
+    <slot name="prefix" />
+    {{ label }}
+    <slot name="suffix" />
+    <button
+      type="button"
+      class="inline-flex items-center p-1 ms-2 text-sm text-blue-400 bg-transparent rounded-sm hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-800 dark:hover:text-blue-300"
+      :class="removeButtonClass"
+      v-if="removeButton"
+      @click="click"
+    >
+      <svg
+        class="w-2 h-2"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 14 14"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+        />
+      </svg>
+    </button>
+  </span>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label?: string;
+  labelClass?: string;
+  class?: string;
+  removeButton?: boolean;
+  removeButtonClass?: string;
+  visibility?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  label: "Default",
+  labelClass: "",
+  class: "",
+  removeButton: true,
+  removeButtonClass: "",
+  visibility: true,
+});
+
+const emit = defineEmits<{
+  (e: "onClick", event: Event): void;
+}>();
+
+const click = (e: Event): void => {
+  emit("onClick", e);
+};
+</script>
