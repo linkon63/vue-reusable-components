3ebe84a4a062dd760e50b17f1648ae471374f089
linkon63 <m.alinkon10@gmail.com>
2024-02-01 13:35:42 +0600

    package created

--- src/components/reusable-components/BaseModal/BaseModal.vue
+++ src/components/reusable-components/BaseModal/BaseModal.vue
@@ -11,7 +11,7 @@
       @keyup.esc="closeWithEscKey"
     >
       <div
-        :class="`${modalSizeClasses[size]}`"
+        :class="`${size}`"
         class="relative p-4 w-full h-full"
       >
         <!-- Modal content -->
@@ -82,19 +82,6 @@ const props = withDefaults(defineProps<ModalProps>(), {
 });
 
 const emit = defineEmits(["close", "click:outside"]);
-const modalSizeClasses = {
-  xs: "max-w-xs",
-  sm: "max-w-sm",
-  md: "max-w-md",
-  lg: "max-w-lg",
-  xl: "max-w-xl",
-  "2xl": "max-w-2xl",
-  "3xl": "max-w-3xl",
-  "4xl": "max-w-4xl",
-  "5xl": "max-w-5xl",
-  "6xl": "max-w-6xl",
-  "7xl": "max-w-7xl",
-};
 
 function closeModal() {
   emit("close");
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import BaseButton from "./components/reusable-components/BaseButton/BaseButton.vue";
+import BaseModal from "./components/reusable-components/BaseModal/BaseModal.vue";
+import BaseToggle from "./components/reusable-components/BaseToggle/BaseToggle.vue";
+import BaseCheckbox from "./components/reusable-components/BaseCheckbox.vue";
+import BaseDropDown from "./components/reusable-components/BaseDropDown.vue";
+import BaseInput from "./components/reusable-components/BaseInput.vue";
+import BaseRadio from "./components/reusable-components/BaseRadio.vue";
+import BaseSelectInput from "./components/reusable-components/BaseSelectInput.vue";
+import BaseTextArea from "./components/reusable-components/BaseTextArea.vue";
+
+export {
+  BaseButton,
+  BaseModal,
+  BaseToggle,
+  BaseCheckbox,
+  BaseDropDown,
+  BaseSelectInput,
+  BaseTextArea,
+  BaseRadio,
+  BaseInput,
+};
